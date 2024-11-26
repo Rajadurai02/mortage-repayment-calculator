37e78c7ab7408a1e487321fd92bbf2d8c43a8793
Rajadurai Sermaraj <Rajadurai.Sermaraj@kanini.com>
2024-11-26 16:16:54 +0530

    Color code gloabally added. All design changes effects and responsive design completed.

--- src/app/mortage-form/mortage-form.component.ts
+++ src/app/mortage-form/mortage-form.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-mortage-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mortage-form.component.html',
   styleUrl: './mortage-form.component.css'
 })
 export class MortageFormComponent {
 
+  formFieldStates : { [key:string] : boolean} = {};
+
+  setFieldState(fieldName:string, state:boolean) {
+    this.formFieldStates[fieldName] = state;
+  }
 }
--- src/app/mortage-payment-result/mortage-payment-result.component.ts
+++ src/app/mortage-payment-result/mortage-payment-result.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-mortage-payment-result',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mortage-payment-result.component.html',
   styleUrl: './mortage-payment-result.component.css'
 })
 export class MortagePaymentResultComponent {
-
+  isPaymentResultCalculated: boolean = false;
 }
